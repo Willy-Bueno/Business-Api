1b149db63a62c4301a89b7cc2d30e737fbd76fb3
Willy-Bueno <willybueno090@gmail.com>
2022-09-07 14:07:20 -0300

    chore: add dotenv

--- src/infra/usecase/db/config/CreateConnection.ts
+++ src/infra/usecase/db/config/CreateConnection.ts
@@ -1,10 +1,14 @@
 import { Sequelize } from 'sequelize'
+import * as dotenv from 'dotenv'
+dotenv.config()
 
 const host = process.env.DB_HOST
 const port = process.env.DB_PORT as unknown as number
 const username = process.env.DB_USERNAME
 const password = process.env.DB_PASSWORD
 const database = process.env.DB_DATABASE
+
+console.log(host, port, username, password, database)
 
 export const sequelize = new Sequelize(database, username, password, {
   host,
